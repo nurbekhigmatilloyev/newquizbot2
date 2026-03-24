e95812f4482ab6643d24e5bce0d5c862762e47b0
nurbekhigmatilloyev <nurbekhigmatilloyev@gmail.com>
2026-03-24 12:10:22 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -128,8 +128,13 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.create_task(main())
-    # Flask serverni ishga tushiramiz (Render portni ko‘rishi uchun)
-    app.run(host="0.0.0.0", port=10000)
+    # Render bergan PORT qiymatini avtomatik olamiz
+    port = int(os.getenv("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
